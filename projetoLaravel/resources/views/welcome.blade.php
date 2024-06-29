e042122016184151038470c23507644f270c63e4
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-06-29 15:32:08 -0300

    adding search
    adicionando a busca e filtragem de eventos no laravel com Event::where e ajustando a UX da pagina.

--- projetoLaravel/resources/views/welcome.blade.php
+++ projetoLaravel/resources/views/welcome.blade.php
@@ -7,13 +7,17 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method="GET">
         <input type="text" name="search" id="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
+    @if ($search)
+        <h2>Buscando por: {{ $search }}</h2>
+    @else
     <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
     <div id="cards-container" >
         @foreach ($events as $event)
             <div class="card col-md-3">
@@ -32,7 +36,9 @@
                 </div>
             </div>
         @endforeach
-        @if (count($events) == 0)
+        @if (count($events) == 0 && $search)
+        <p>Não foi possivel encontrar nenhum evento com: {{ $search }}! <a href="/">Ver todos!</a></p>
+        @elseif(count($events) == 0)
             <p>Não há eventos disponíveis no momento!</p>
         @endif
     </div>
