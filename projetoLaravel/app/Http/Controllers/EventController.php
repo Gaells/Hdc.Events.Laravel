63debadf8dad0370ea3c4b58f3935fdff182159d
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-07-04 20:42:28 -0300

    many to many relationship
    contexto: um usuário participa de um evento e um evento tem vários participantes
    
    criação das migrations e funções do controller, alem da rota

--- projetoLaravel/app/Http/Controllers/EventController.php
+++ projetoLaravel/app/Http/Controllers/EventController.php
@@ -134,6 +134,18 @@ class EventController extends Controller
 
     }
 
+    public function joinEvent($id) {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->attach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no eventos' . $event->title);
+
+    }
+
 }
 
 
